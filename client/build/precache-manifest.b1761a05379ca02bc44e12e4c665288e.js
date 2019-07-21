2765f381f2df2b66121fe2a2f8bcf7b0c203fd99
pawansa3 <pawansa3@noreply.github.com>
2019-07-21 11:45:31 +0530

    build deployment

---
+++ client/build/precache-manifest.b1761a05379ca02bc44e12e4c665288e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2d0b576dca6aefce2d4729207ad83e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a40e8068ea6d4afac846",
+    "url": "/static/js/2.83752851.chunk.js"
+  },
+  {
+    "revision": "67fcca71b0d71119bee7",
+    "url": "/static/js/main.c165c98d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
